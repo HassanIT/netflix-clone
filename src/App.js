848023837bfd7b898f8332a815717a26518a1ada
Hassan Nisar <development.sentients@aleeas.com>
2024-05-26 16:40:12 -0400

    implemented login & logout with redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // Logged out
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,20 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDqYMNg4BdPM6CSnfBvivve1UDlx031s38",
   authDomain: "netflix-clone-8ee2b.firebaseapp.com",
   projectId: "netflix-clone-8ee2b",
   storageBucket: "netflix-clone-8ee2b.appspot.com",
   messagingSenderId: "680803273649",
-  appId: "1:680803273649:web:d69150121a3d68c9618b90"
+  appId: "1:680803273649:web:d69150121a3d68c9618b90",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+// We can have many explicit exports, but only one default
+export { auth };
+export default db;
